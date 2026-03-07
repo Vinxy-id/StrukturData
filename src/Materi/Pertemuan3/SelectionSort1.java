ae3639b098237059fc4fa3bc266807b85e8551bd
Vinxy-id <alvinwidiantara2019@gmail.com>
2026-03-08 03:27:01 +0700

    style: format code and fix loop condition in SelectionSort1

--- src/Materi/Pertemuan3/SelectionSort1.java
+++ src/Materi/Pertemuan3/SelectionSort1.java
@@ -1,12 +1,17 @@
 package Materi.Pertemuan3;
-public class SelectionSort1{
+
+public class SelectionSort1 {
     public static void main(String[] args) {
-        int[]x={20,15,90,13,26};
+        int[] x = {20, 15, 90, 13, 26};
         int a = 0;
-        a = x[0]; 
-        x[0]=x[4];
-        x[4]=a;
-        for(int i=0;i>x.length;i++){
+
+        // Manual swap example for index 0 and 4
+        a = x[0];
+        x[0] = x[4];
+        x[4] = a;
+
+        // Corrected loop condition: use < instead of >
+        for (int i = 0; i < x.length; i++) {
             System.out.println(x[i]);
         }
     }
